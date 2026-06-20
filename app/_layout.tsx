fce250c467f08fc7dd37ceb0e7ad770367c8675c
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-06-20 08:19:13 -0500

    Iniciando proyecto

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,18 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import "../global.css"
+import { View ,Text} from 'react-native';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        iniciando el examen parcial!
+      </Text>
+      <Text className="text-xl font-bold text-red-500">
+        con react native y tailwind
+      </Text>
+    </View>
   );
 }
