8e353177736453f1b579061bb28d1e38a1d6d00f
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-06-21 16:36:27 -0500

    feat: agregando pagina de registro

--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,43 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
+import { Link } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 bg-white ">
-
       <View className="h-[60%] w-full justify-center items-center">
         <Image
-          source={require('../assets/fondo-pantalla-inicio.png')}
+          source={require("../assets/fondo-pantalla-inicio.png")}
           className="w-full h-full"
           resizeMode="cover"
         />
       </View>
       <View className="h-[40%] w-full  items-center px-6 pt-8 pb-12">
-
         <View className="items-center">
           <Text className=" text-3xl font-extrabold text-center mb-2">
             ¡Bienvenido!
           </Text>
           <Text className=" text-base text-center px-4 mb-6">
-            Explora nuestra tienda y encuentra los mejores productos con un solo lugar
+            Explora nuestra tienda y encuentra los mejores productos con un solo
+            lugar
           </Text>
         </View>
-        <TouchableOpacity
-          className="w-6/12 rounded-2xl overflow-hidden active:opacity-90 shadow-lg"
-          onPress={() => console.log('Botón presionado')}
-        >
-          <LinearGradient
-            colors={['#4E65CC', '#958FD9']}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 0 }}
-            className="py-4 rounded-2xl"
-          >
-            <Text className="text-white text-center font-bold text-lg">
-              Comenzar
-            </Text>
-          </LinearGradient>
-        </TouchableOpacity>
-      </View>
 
+        <Link href="/login" asChild>
+          <TouchableOpacity className="w-6/12 rounded-2xl overflow-hidden active:opacity-90 shadow-lg">
+            <LinearGradient
+              colors={["#4E65CC", "#958FD9"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              className="py-4 rounded-2xl"
+            >
+              <Text className="text-white text-center font-bold text-lg">
+                Comenzar
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </Link>
+      </View>
     </View>
   );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { Link } from 'expo-router';
+import { useState } from 'react'
+import { Text, TextInput, View, Pressable ,Image} from 'react-native'
 
-function login() {
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+
+  const [password, setPassword] = useState('');
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+
   return (
-    <div>login</div>
-  )
-}
+  <View className="flex-1 justify-center items-center bg-white px-4">
+    <View className="w-full  px-6  pb-12 items-center">
+      
+      <Image
+        source={require("../assets/logoCyber-r.png")}
+        className="w-[200px] h-[200px] mb-1" // Ajustado tamaño para que no ocupe toda la pantalla al no tener el contenedor anterior
+        resizeMode="contain"
+      />
 
-export default login
+      <Text className='mb-2 font-bold text-3xl text-center text-gray-500'>
+        Bienvenido de Nuevo
+      </Text>
+
+      <Text className='mb-12 text-sm text-center text-gray-400'>
+        Ingresa tus datos para iniciar sesión
+      </Text>
+
+      {/* --- PRIMER INPUT: CORREO --- */}
+      <View className="relative w-11/12 mb-8">
+        <View className="absolute -top-2.5 left-4 bg-white px-1 z-10">
+          <Text className={`text-xs font-medium ${isEmailFocused ? 'text-blue-600' : 'text-gray-500'}`}>
+            Correo electrónico
+          </Text>
+        </View>
+        <TextInput
+          className={`w-full border rounded-2xl p-3.5 text-base text-gray-800 ${isEmailFocused ? 'border-blue-600' : 'border-gray-300'
+            }`}
+          value={email}
+          onChangeText={setEmail}
+          onFocus={() => setIsEmailFocused(true)}
+          onBlur={() => setIsEmailFocused(false)}
+          placeholder="ejemplo@correo.com"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+      </View>
+
+      {/* --- SEGUNDO INPUT: CONTRASEÑA --- */}
+      <View className="relative w-11/12 mb-8">
+        <View className="absolute -top-2.5 left-4 bg-white px-1 z-10">
+          <Text className={`text-xs font-medium ${isPasswordFocused ? 'text-blue-600' : 'text-gray-500'}`}>
+            Contraseña
+          </Text>
+        </View>
+        <TextInput
+          className={`w-full border rounded-2xl p-3.5 text-base text-gray-800 ${isPasswordFocused ? 'border-blue-600' : 'border-gray-300'
+            }`}
+          value={password}
+          onChangeText={setPassword}
+          onFocus={() => setIsPasswordFocused(true)}
+          onBlur={() => setIsPasswordFocused(false)}
+          placeholder="••••••••"
+          secureTextEntry={true}
+        />
+      </View>
+
+      {/* --- BOTÓN DE INICIAR SESIÓN (Sin Links temporales) --- */}
+      <Pressable className="w-11/12 py-3 rounded-2xl bg-[#4C5AE0] active:opacity-90">
+        <Text className="text-center text-white font-bold text-sm">
+          Iniciar Sesión
+        </Text>
+      </Pressable>
+
+      <View className="flex-row justify-center items-center mt-6">
+        <Text className="text-gray-500 text-sm">
+          ¿No tienes cuenta?{" "}
+        </Text>
+
+        {/* El Link ahora se ve azul, en negrita y reacciona al tocarlo */}
+        <Link href="/registro" className="text-blue-500 font-bold text-sm active:opacity-70">
+          Regístrate
+        </Link>
+      </View>
+
+    </View>
+  </View>
+);
+}
